80921a4fd2f7e58d9a191564662e8ac59f55bdcd
litcoderr <litcoderr@gmail.com>
2022-11-02 11:05:30 +0900

    added docker

---
+++ docker/launch.sh
@@ -0,0 +1,10 @@
+cd ..
+CUDA_VISIBLE_DEVICES='2,5'
+
+docker run --gpus '"'device=$CUDA_VISIBLE_DEVICES'"' --ipc=host --rm -it \
+    --mount src=$(pwd),dst=/CLIP,type=bind \
+    --mount src=/media/data,dst=/data,type=bind \
+    -e NVIDIA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
+    -w /CLIP \
+    litcoderr/clip_laplacian:latest \
+    bash -c "bash" \
